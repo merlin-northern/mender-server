24d462fde04e49067074e81aa7031f709d433893
Peter Grzybowski <peter@northern.tech>
2026-02-02 21:10:37 +0100

    chore: fixes 13
    Ticket: None
    Signed-off-by: Peter Grzybowski <peter@northern.tech>

--- backend/tests/runner/tests/login_test.go
+++ backend/tests/runner/tests/login_test.go
@@ -31,7 +31,6 @@ func mainTestLogin(t *testing.T, settings *TestSettings) error {
 	if err != nil {
 		return errors.Wrap(err, "failed to create client")
 	}
-	settings.client = c
 
 	basicAuth := b64.StdEncoding.EncodeToString([]byte(settings.Username + ":" + settings.Password))
 	l, err := c.LoginWithResponse(
@@ -61,6 +60,22 @@ func mainTestLogin(t *testing.T, settings *TestSettings) error {
 	assert.NoError(t, err)
 	assert.Equal(t, 200, m.HTTPResponse.StatusCode)
 	assert.Equal(t, settings.Username, m.JSON200.Email)
+
+	httpClient = &http.Client{
+		Transport: &http.Transport{
+			TLSClientConfig: &tls.Config{
+				InsecureSkipVerify: true,
+			},
+		},
+	}
+	settings.client, err = client.NewClientWithResponses(
+		settings.ServerURL,
+		client.WithHTTPClient(httpClient),
+		client.WithRequestEditorFn(func(ctx context.Context, req *http.Request) error {
+			req.Header.Set("Authorization", "Bearer "+settings.jwt)
+			return nil
+		}),
+	)
 
 	t.Logf("test passed with user data: %+v\n", m.JSON200)
 	return nil
